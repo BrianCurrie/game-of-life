da34c03c939270c039d93b5cbbc9fb8af4d2f0f5
Brian Currie <brian.currie@hotmail.com>
2021-06-04 16:10:11 -0700

    Mobile Vh now called once on page load

--- scripts.js
+++ scripts.js
@@ -28,10 +28,16 @@ createGrid(gridSize);
 createPresetListeners();
 
 /* Listener to make sure page is always 100vh on mobile */
-window.addEventListener("resize", () => {
+function setVh() {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty("--vh", `${vh}px`);
+}
+
+window.addEventListener("resize", () => {
+    setVh();
 });
+
+setVh();
 
 /***************************
 
